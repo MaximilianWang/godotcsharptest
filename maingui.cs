9650997bd51779256d0547902a61b7d3e1b2980e
D W <h.k.lieraenoire@gmail.com>
2018-04-02 21:53:24 +1000

    inital commit of test project. csharp part not building properly

---
+++ maingui.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public class maingui : CanvasLayer
+{
+    // Member variables here, example:
+    // private int a = 2;
+    // private string b = "textvar";
+
+    public override void _Ready()
+    {
+        // Called every time the node is added to the scene.
+        // Initialization here
+        
+    }
+
+    private void _on_exit_pressed()
+    {
+        // Replace with function body
+        GetTree().Quit();
+		
+    }
+
+
+//    public override void _Process(float delta)
+//    {
+//        // Called every frame. Delta is time since last frame.
+//        // Update game logic here.
+//        
+//    }
+}
+
+
---
+++ splash.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+public class splash : Control
+{
+    // Member variables here, example:
+    // private int a = 2;
+    // private string b = "textvar";
+
+    public override void _Ready()
+    {  
+        // Called every time the node is added to the scene.
+        // Initialization here
+        
+    }
+
+
+    private void _on_Button_pressed()
+    {
+        // Replace with function body
+        GD.Print("HI");
+        //var nextScene = G
+        
+		//((global)GetNode("/root/global")).GoToSchene("res://game.tscn", "level-01");
+        
+        
+    }
+
+
+//    public override void _Process(float delta)
+//    {
+//        // Called every frame. Delta is time since last frame.
+//        // Update game logic here.
+//        
+//    }
+}
+
